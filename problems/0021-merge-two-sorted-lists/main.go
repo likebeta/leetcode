3da4bc1044de8a0809f7608ce53b7d54dc756b8e
likebeta <ixxoo.me@gmail.com>
2020-05-12 21:30:45 +0800

    21

---
+++ problems/0021-merge-two-sorted-lists/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"leetcode/helper"
+)
+
+type ListNode = helper.ListNode
+
+// 合并两个有序链表
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	var l ListNode
+	p := &l
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			p.Next = l1
+			p, l1 = l1, l1.Next
+		} else {
+			p.Next = l2
+			p, l2 = l2, l2.Next
+		}
+	}
+
+	if l1 != nil {
+		p.Next = l1
+	} else if l2 != nil {
+		p.Next = l2
+	}
+
+	return l.Next
+}
+
+func main() {
+	l1 := helper.NewList([]int{1, 2, 4})
+	l2 := helper.NewList([]int{1, 3, 4})
+	l := mergeTwoLists(l1, l2)
+	helper.Log(l.ToArray())
+}
