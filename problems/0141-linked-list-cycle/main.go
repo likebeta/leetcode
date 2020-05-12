0521f4ba4b92fef323b85c95d69b876d034a91fd
likebeta <ixxoo.me@gmail.com>
2020-05-12 21:32:10 +0800

    141

---
+++ problems/0141-linked-list-cycle/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"leetcode/helper"
+)
+
+type ListNode = helper.ListNode
+
+// 环形链表
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+
+	slow, fast := head, head.Next
+	for fast != slow && fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return fast == slow
+}
+
+func main() {
+	var head *ListNode
+	head = helper.NewCycleList([]int{3, 2, 0, -4}, 1)
+	helper.Assert(hasCycle(head) == true)
+
+	head = helper.NewCycleList([]int{1, 2}, 1)
+	helper.Assert(hasCycle(head) == true)
+
+	head = helper.NewCycleList([]int{1}, -1)
+	helper.Assert(hasCycle(head) == false)
+}
