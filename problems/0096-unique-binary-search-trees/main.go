f22f14654dfcb1bb3de6507e35dc351364e10eff
likebeta <ixxoo.me@gmail.com>
2020-05-21 23:25:12 +0800

    96

---
+++ problems/0096-unique-binary-search-trees/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"leetcode/helper"
+)
+
+// 不同的二叉搜索树
+func numtrees(n int) int {
+	// dp(n) = f(1) + f(2) + ... + f(i) + ... + f(n)
+	// f(i) = dp(i-1)*dp(n-i)
+	dp := make([]int, n+1, n+1)
+	dp[0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
+
+func main() {
+	helper.Assert(numtrees(3) == 5)
+}
