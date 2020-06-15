56300db9ce22b2c7d797077c80198012afff9d6a
likebeta <ixxoo.me@gmail.com>
2020-06-15 23:38:21 +0800

    14

---
+++ problems/0014-longest-common-prefix/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "leetcode/helper"
+
+// 最长公共前缀
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+	return strs[0]
+}
+
+func main() {
+	helper.Assert(longestCommonPrefix([]string{"flower", "flow", "flight"}) == "fl")
+	helper.Assert(longestCommonPrefix([]string{"dog", "racecar", "car"}) == "")
+}
