3d72450007ce1b219edd320d38cd0972f616d711
likebeta <ixxoo.me@gmail.com>
2020-06-29 22:57:09 +0800

    215

---
+++ problems/0215-kth-largest-element-in-an-array/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"leetcode/helper"
+)
+
+// 数组中的第K个最大元素
+func findKthLargest(nums []int, k int) int {
+	heapSize := len(nums)
+	for i := heapSize / 2; i >= 0; i-- {
+		maxHeapify(nums, i, heapSize)
+	}
+	for i := 1; i < k; i++ {
+		heapSize--
+		nums[0], nums[heapSize] = nums[heapSize], nums[0]
+		maxHeapify(nums, 0, heapSize)
+	}
+	return nums[0]
+}
+
+func maxHeapify(a []int, i, heapSize int) {
+	l, r, largest := i*2+1, i*2+2, i
+	if l < heapSize && a[l] > a[largest] {
+		largest = l
+	}
+	if r < heapSize && a[r] > a[largest] {
+		largest = r
+	}
+	if largest != i {
+		a[i], a[largest] = a[largest], a[i]
+		maxHeapify(a, largest, heapSize)
+	}
+}
+
+func main() {
+	helper.Assert(findKthLargest([]int{3, 2, 1, 5, 6, 4}, 5) == 2)
+	helper.Assert(findKthLargest([]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4) == 4)
+}
