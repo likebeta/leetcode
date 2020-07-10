4d7f1ac2118fa1f2ce9a768630b1b73a93e1f69b
likebeta <ixxoo.me@gmail.com>
2020-07-10 23:58:24 +0800

    v2

--- problems/0309-best-time-to-buy-and-sell-stock-with-cooldown/main.go
+++ problems/0309-best-time-to-buy-and-sell-stock-with-cooldown/main.go
@@ -5,12 +5,12 @@ import (
 )
 
 // 最佳买卖股票时机含冷冻期
+/*
+   第n天买入的时候, 要从n-2那天的状态转移, 不再是n-1
+   dp[n].empty = max(dp[n-1].empty, dp[n-1].full + prices[n])
+   dp[n].full  = max(dp[n-1].full, dp[n-2].empty - prices[n])
+*/
 func maxProfit(prices []int) int {
-	/*
-	   第n天买入的时候, 要从n-2那天的状态转移, 不再是n-1
-	   dp[n].empty = max(dp[n-1].empty, dp[n-1].full + prices[n])
-	   dp[n].full  = max(dp[n-1].full, dp[n-2].empty - prices[n])
-	*/
 	N := len(prices)
 	if N == 0 {
 		return 0
@@ -25,7 +25,23 @@ func maxProfit(prices []int) int {
 		dp[n].Full = max(dp[n-1].Full, prePreEmpty-prices[n])
 		prePreEmpty = dp[n-1].Empty
 	}
-	return max(dp[N-1].Full, dp[N-1].Empty)
+	return dp[N-1].Empty
+}
+
+func maxProfit2(prices []int) int {
+	N := len(prices)
+	if N == 0 {
+		return 0
+	}
+	prePreEmpty := 0
+	pre := Equities{Empty: 0, Full: -prices[0]}
+	for n := 1; n < N; n++ {
+		tmp := pre
+		pre.Empty = max(tmp.Empty, tmp.Full+prices[n])
+		pre.Full = max(tmp.Full, prePreEmpty-prices[n])
+		prePreEmpty = tmp.Empty
+	}
+	return pre.Empty
 }
 
 type Equities struct {
@@ -40,6 +56,11 @@ func max(a, b int) int {
 	return a
 }
 
+func testOne(arr []int, ans int) {
+	helper.Assert(maxProfit(arr) == ans)
+	helper.Assert(maxProfit2(arr) == ans)
+}
+
 func main() {
-	helper.Assert(maxProfit([]int{1, 2, 3, 0, 2}) == 3)
+	testOne([]int{1, 2, 3, 0, 2}, 3)
 }
