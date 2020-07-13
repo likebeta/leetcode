e3eab2fb0b1bea76f4f16c85d0edcf85bc48da96
likebeta <ixxoo.me@gmail.com>
2020-07-13 22:49:11 +0800

    350

--- problems/0350-intersection-of-two-arrays-ii/main.go
+++ problems/0350-intersection-of-two-arrays-ii/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"leetcode/helper"
+	"sort"
 )
 
 // 两个数组的交集 II
@@ -24,7 +25,33 @@ func intersect(nums1 []int, nums2 []int) []int {
 	return ans
 }
 
+func intersect2(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	N1, N2 := len(nums1), len(nums2)
+	index1, index2 := 0, 0
+
+	var ans []int
+	for index1 < N1 && index2 < N2 {
+		if nums1[index1] < nums2[index2] {
+			index1++
+		} else if nums1[index1] > nums2[index2] {
+			index2++
+		} else {
+			ans = append(ans, nums1[index1])
+			index1++
+			index2++
+		}
+	}
+	return ans
+}
+
+func testOne(arr1, arr2, ans []int) {
+	helper.Log(intersect(arr1, arr2), ans)
+	helper.Log(intersect2(arr1, arr2), ans)
+}
+
 func main() {
-	helper.Log(intersect([]int{1, 2, 2, 1}, []int{2, 2}), []int{2, 2})
-	helper.Log(intersect([]int{4, 9, 5}, []int{9, 4, 9, 8, 4}), []int{4, 9})
+	testOne([]int{1, 2, 2, 1}, []int{2, 2}, []int{2, 2})
+	testOne([]int{4, 9, 5}, []int{9, 4, 9, 8, 4}, []int{4, 9})
 }
