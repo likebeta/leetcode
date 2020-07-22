0c60d82f6f8d49bbb40b6bd302079284cdc5ca22
likebeta <ixxoo.me@gmail.com>
2020-07-22 23:31:51 +0800

    153

--- problems/0153-find-minimum-in-rotated-sorted-array/main.go
+++ problems/0153-find-minimum-in-rotated-sorted-array/main.go
@@ -6,9 +6,9 @@ import (
 
 func findMin(nums []int) int {
 	i, j := 0, len(nums)-1
-	for i <= j && nums[i] > nums[j] {
+	for i < j {
 		mid := i + (j-i)/2
-		if nums[mid] >= nums[i] {
+		if nums[mid] > nums[j] {
 			i = mid + 1
 		} else {
 			j = mid
