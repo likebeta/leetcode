e3b870dfcfb16e894e4a316db5f777fbe04a5a39
likebeta <ixxoo.me@gmail.com>
2020-08-09 01:45:57 +0800

    99

--- problems/0099-recover-binary-search-tree/main.go
+++ problems/0099-recover-binary-search-tree/main.go
@@ -7,10 +7,10 @@ import (
 type TreeNode = helper.TreeNode
 
 // 恢复二叉搜索树
-func recoverTree(root *TreeNode)  {
+func recoverTree(root *TreeNode) {
 	var (
 		node1, node2, pre *TreeNode
-		inOrder func(node *TreeNode)
+		inOrder           func(node *TreeNode)
 	)
 
 	inOrder = func(node *TreeNode) {
@@ -33,9 +33,44 @@ func recoverTree(root *TreeNode)  {
 	node1.Val, node2.Val = node2.Val, node1.Val
 }
 
+func recoverTree2(root *TreeNode) {
+	var node1, node2, pre *TreeNode
+	for root != nil {
+		needCheck := false
+		if root.Left == nil {
+			needCheck = true
+		} else {
+			leftMostRight := root.Left
+			for leftMostRight.Right != nil && leftMostRight.Right != root {
+				leftMostRight = leftMostRight.Right
+			}
+			if leftMostRight.Right == root {
+				needCheck = true
+				leftMostRight.Right = nil
+			} else {
+				leftMostRight.Right = root
+				root = root.Left
+			}
+		}
+		if needCheck {
+			if pre != nil && pre.Val > root.Val {
+				node2 = root
+				if node1 == nil {
+					node1 = pre
+				}
+			}
+			pre, root = root, root.Right
+		}
+	}
+	node1.Val, node2.Val = node2.Val, node1.Val
+}
+
 func testOne(in string, out string) {
 	inTree := helper.NewTree(in)
 	recoverTree(inTree)
+	helper.Assert(inTree.Dump() == out)
+	inTree = helper.NewTree(in)
+	recoverTree2(inTree)
 	helper.Assert(inTree.Dump() == out)
 }
 
@@ -43,4 +78,5 @@ func main() {
 	testOne("[1,3,null,null,2]", "[3,1,null,null,2]")
 	testOne("[3,1,4,null,null,2]", "[2,1,4,null,null,3]")
 	testOne("[2,1,4,null,null,5,3]", "[2,1,4,null,null,3,5]")
+	testOne("[10,5,15,0,8,13,20,2,-5,6,9,12,14,18,25]", "[10,5,15,0,8,13,20,-5,2,6,9,12,14,18,25]")
 }
