2feb22bcb3701cc72cac16a7190035a097af61e6
likebeta <ixxoo.me@gmail.com>
2020-08-19 13:43:17 +0800

    109

--- problems/0109-convert-sorted-list-to-binary-search-tree/main.go
+++ problems/0109-convert-sorted-list-to-binary-search-tree/main.go
@@ -9,35 +9,72 @@ type TreeNode = helper.TreeNode
 
 // 有序链表转换二叉搜索树
 func sortedListToBST(head *ListNode) *TreeNode {
+	var (
+		getMedian func(*ListNode, *ListNode) *ListNode
+		buildTree func(*ListNode, *ListNode) *TreeNode
+	)
+
+	getMedian = func(left *ListNode, right *ListNode) *ListNode {
+		fast, slow := left, left
+		for fast != right && fast.Next != right {
+			slow = slow.Next
+			fast = fast.Next.Next
+		}
+		return slow
+	}
+
+	buildTree = func(left *ListNode, right *ListNode) *TreeNode {
+		if left == right {
+			return nil
+		}
+		mid := getMedian(left, right)
+		root := &TreeNode{Val: mid.Val}
+		root.Left = buildTree(left, mid)
+		root.Right = buildTree(mid.Next, right)
+		return root
+	}
+
 	return buildTree(head, nil)
 }
 
-func getMedian(left, right *ListNode) *ListNode {
-	fast, slow := left, left
-	for fast != right && fast.Next != right {
-		slow = slow.Next
-		fast = fast.Next.Next
+func sortedListToBST2(head *ListNode) *TreeNode {
+	var (
+		globalHead = head
+		getLength  func(*ListNode) int
+		buildTree  func(int, int) *TreeNode
+	)
+	getLength = func(head *ListNode) int {
+		length := 0
+		for curr := head; curr != nil; curr = curr.Next {
+			length++
+		}
+		return length
 	}
-	return slow
+
+	buildTree = func(left int, right int) *TreeNode {
+		if left > right {
+			return nil
+		}
+
+		mid := left + (right-left)/2
+		root := &TreeNode{}
+		root.Left = buildTree(left, mid-1)
+		root.Val = globalHead.Val
+		globalHead = globalHead.Next
+		root.Right = buildTree(mid+1, right)
+		return root
+	}
+	return buildTree(0, getLength(head)-1)
 }
 
-func buildTree(left, right *ListNode) *TreeNode {
-	if left == right {
-		return nil
-	}
-	mid := getMedian(left, right)
-	root := &TreeNode{Val: mid.Val}
-	root.Left = buildTree(left, mid)
-	root.Right = buildTree(mid.Next, right)
-	return root
-}
-
-func testOne(in []int) {
-	list := helper.NewList(in)
-	t := sortedListToBST(list)
-	t.Print(8)
+func testOne(in string, ans string) {
+	list := helper.ParseList(in)
+	t1 := sortedListToBST(list)
+	helper.Assert(t1.Dump() == ans)
+	t2 := sortedListToBST2(list)
+	helper.Log(t2.Dump(), ans)
 }
 
 func main() {
-	testOne([]int{-10, -3, 0, 5, 9})
+	testOne("[-10,-3,0,5,9]", "[0,-3,9,-10,null,5]")
 }
