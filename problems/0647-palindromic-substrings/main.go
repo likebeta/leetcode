eab7c4d0304dc353dbb5f525af0c9eb3ad52a7fc
likebeta <ixxoo.me@gmail.com>
2020-08-20 13:31:38 +0800

    647

--- problems/0647-palindromic-substrings/main.go
+++ problems/0647-palindromic-substrings/main.go
@@ -6,6 +6,28 @@ import (
 
 // 回文子串
 func countSubstrings(s string) int {
+	var (
+		n                = len(s)
+		ans              int
+		countPalindromic func(l, r int)
+	)
+	countPalindromic = func(l, r int) {
+		for l >= 0 && r < n && s[l] == s[r] {
+			l--
+			r++
+			ans++
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		countPalindromic(i, i)
+		countPalindromic(i-1, i)
+	}
+	return ans
+}
+
+// 归一化
+func countSubstrings2(s string) int {
 	n := len(s)
 	ans := 0
 	for i := 0; i < 2*n-1; i++ {
@@ -19,7 +41,60 @@ func countSubstrings(s string) int {
 	return ans
 }
 
+// dp
+func countSubstrings3(s string) int {
+	n := len(s)
+	dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, n)
+	}
+	ans := n
+	for col := 0; col < n; col++ {
+		dp[col][col] = true
+		for row := 0; row < col; row++ {
+			if col == row + 1 {
+				dp[row][col] = s[row] == s[col]
+			} else {
+				dp[row][col] = dp[row+1][col-1] && s[row] == s[col]
+			}
+			if dp[row][col] {
+				ans++
+			}
+		}
+	}
+	return ans
+}
+
+func countSubstrings4(s string) int {
+	n := len(s)
+	dp := make([]bool, n)
+	ans := n
+	for col := 0; col < n; col++ {
+		dp[col] = true
+		for row := 0; row < col; row++ {
+			if col == row + 1 {
+				dp[row] = s[row] == s[col]
+			} else {
+				dp[row] = dp[row+1] && s[row] == s[col]
+			}
+			if dp[row] {
+				ans++
+			}
+		}
+	}
+	return ans
+}
+
+func testOne(s string, ans int) {
+	helper.Assert(countSubstrings(s) == ans)
+	helper.Assert(countSubstrings2(s) == ans)
+	helper.Assert(countSubstrings3(s) == ans)
+	helper.Assert(countSubstrings4(s) == ans)
+}
+
 func main() {
-	helper.Assert(countSubstrings("abc") == 3)
-	helper.Assert(countSubstrings("aaa") == 6)
+	testOne("abc", 3)
+	testOne("aaa", 6)
+	testOne("abcba", 7)
+	testOne("fdsklf", 6)
 }
