8d9fb679d2df32ee1283af79d8c05fd63a769199
likebeta <ixxoo.me@gmail.com>
2020-09-15 23:33:20 +0800

    37

---
+++ problems/0037-sudoku-solver/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"leetcode/helper"
+)
+
+// 解数独
+func solveSudoku(board [][]byte) {
+	var (
+		line   [9][9]bool
+		column [9][9]bool
+		block  [3][3][9]bool
+		spaces [][2]int
+		dfs    func(int) bool
+	)
+
+	for i, row := range board {
+		for j, b := range row {
+			if b == '.' {
+				spaces = append(spaces, [2]int{i, j})
+			} else {
+				digit := b - '1'
+				line[i][digit] = true
+				column[j][digit] = true
+				block[i/3][j/3][digit] = true
+			}
+		}
+	}
+
+	dfs = func(pos int) bool {
+		if pos == len(spaces) {
+			return true
+		}
+		i, j := spaces[pos][0], spaces[pos][1]
+		for digit := byte(0); digit < 9; digit++ {
+			if !line[i][digit] && !column[j][digit] && !block[i/3][j/3][digit] {
+				line[i][digit] = true
+				column[j][digit] = true
+				block[i/3][j/3][digit] = true
+				board[i][j] = digit + '1'
+				if dfs(pos + 1) {
+					return true
+				}
+				line[i][digit] = false
+				column[j][digit] = false
+				block[i/3][j/3][digit] = false
+			}
+		}
+		return false
+	}
+	dfs(0)
+}
+
+func main() {
+	board := [][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	}
+	solveSudoku(board)
+	for i := range board {
+		helper.Log(string(board[i]))
+	}
+}
