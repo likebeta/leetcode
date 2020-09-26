0207cbd210f20ff08462edcbd854cbf8a8b50755
likebeta <ixxoo.me@gmail.com>
2020-09-26 15:31:12 +0800

    113

--- problems/0113-path-sum-ii/main.go
+++ problems/0113-path-sum-ii/main.go
@@ -8,32 +8,33 @@ type TreeNode = helper.TreeNode
 
 // 路径总和 II
 func pathSum(root *TreeNode, sum int) [][]int {
-	return getPath(root, sum, nil, nil)
-}
+	var (
+		ans     [][]int
+		getPath func(*TreeNode, int, []int)
+	)
 
-func getPath(root *TreeNode, sum int, result [][]int, path []int) [][]int {
-	if root == nil {
-		return result
-	}
-
-	sum -= root.Val
-	path = append(path, root.Val)
-	if root.Left == nil && root.Right == nil {
-		if sum == 0 {
-			tmp := make([]int, len(path), len(path))
-			copy(tmp, path)
-			result = append(result, tmp)
+	getPath = func(node *TreeNode, sum int, record []int) {
+		if node != nil {
+			sum -= node.Val
+			record = append(record, node.Val)
+			if node.Left == nil && node.Right == nil {
+				if sum == 0 {
+					tmp := make([]int, len(record))
+					copy(tmp, record)
+					ans = append(ans, tmp)
+				}
+			} else {
+				getPath(node.Left, sum, record)
+				getPath(node.Right, sum, record)
+			}
 		}
-		return result
 	}
-	result = getPath(root.Left, sum, result, path)
-	result = getPath(root.Right, sum, result, path)
-	return result
+
+	getPath(root, sum, nil)
+	return ans
 }
 
 func main() {
-	var node *TreeNode
-	node = helper.NewTree("[5,4,8,11,null,13,4,7,2,null,null,5,1]")
-	helper.PrintTree(node, 8)
-	helper.Log(pathSum(node, 22)) // [[5 4 11 2] [5 8 4 5]]
+	t := helper.NewTree("[5,4,8,11,null,13,4,7,2,null,null,5,1]")
+	helper.Log(pathSum(t, 22)) // [[5 4 11 2] [5 8 4 5]]
 }
