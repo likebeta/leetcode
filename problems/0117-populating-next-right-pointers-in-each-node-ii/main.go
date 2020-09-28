7acf31387490d016d5439441cd1aa38f3b331495
likebeta <ixxoo.me@gmail.com>
2020-09-28 23:45:40 +0800

    117

---
+++ problems/0117-populating-next-right-pointers-in-each-node-ii/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+// 填充每个节点的下一个右侧节点指针 II
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	for start := root; start != nil; {
+		var first, prev *Node
+		for curr := start; curr != nil; curr = curr.Next {
+			if curr.Left != nil {
+				if prev == nil {
+					first, prev = curr.Left, curr.Left
+				} else {
+					prev.Next, prev = curr.Left, curr.Left
+				}
+			}
+			if curr.Right != nil {
+				if prev == nil {
+					first, prev = curr.Right, curr.Right
+				} else {
+					prev.Next, prev = curr.Right, curr.Right
+
+				}
+			}
+		}
+		start = first
+	}
+	return root
+}
+
+func parse(bfs string) *Node {
+	var arr []*int
+	if err := json.Unmarshal([]byte(bfs), &arr); err != nil {
+		log.Panic("parse failed:", err)
+	}
+
+	var root *Node
+	cnt := len(arr)
+	if cnt > 0 {
+		root = &Node{Val: *arr[0]}
+		arr = arr[1:]
+		list := []*Node{root}
+		for len(list) > 0 && len(arr) > 0 {
+			curr := list[0]
+			list = list[1:]
+			if len(arr) > 0 {
+				if arr[0] != nil {
+					curr.Left = &Node{Val: *arr[0]}
+					list = append(list, curr.Left)
+				}
+				arr = arr[1:]
+			}
+
+			if len(arr) > 0 {
+				if arr[0] != nil {
+					curr.Right = &Node{Val: *arr[0]}
+					list = append(list, curr.Right)
+				}
+				arr = arr[1:]
+			}
+		}
+	}
+	return root
+}
+
+
+func main() {
+	node := parse("[1,2,3,4,5,null,7]")
+	node = connect(node)
+}
