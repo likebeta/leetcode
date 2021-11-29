c2b8eaf0660e9c96b3379849474d01a04cd92fef
likebeta <ixxoo.me@gmail.com>
2021-11-29 16:23:47 +0800

    739

--- problems/0739-daily-temperatures/main.go
+++ problems/0739-daily-temperatures/main.go
@@ -8,10 +8,9 @@ import (
 func dailyTemperatures(T []int) []int {
 	ans := make([]int, len(T))
 	stack := make([]int, len(T)+1)
-	stack[0] = -1
 	var k int
 	for i := range T {
-		for top := stack[k]; top != -1 && T[top] < T[i]; top = stack[k] {
+		for top := stack[k]; k != 0 && T[top] < T[i]; top = stack[k] {
 			ans[top], k = i-top, k-1
 		}
 		k++
@@ -20,6 +19,15 @@ func dailyTemperatures(T []int) []int {
 	return ans
 }
 
+func testOne(in string, ans string) {
+	nums := helper.ParseArray(in)
+	result := dailyTemperatures(nums)
+	helper.Assert(helper.DumpArray(result) == ans)
+	helper.Log(in, "=>", ans)
+}
+
 func main() {
-	helper.Log(dailyTemperatures([]int{73, 74, 75, 71, 69, 72, 76, 73}), []int{1, 1, 4, 2, 1, 1, 0, 0})
+	testOne("[73,74,75,71,69,72,76,73]", "[1,1,4,2,1,1,0,0]")
+	testOne("[30,40,50,60]", "[1,1,1,0]")
+	testOne("[30,60,90]", "[1,1,0]")
 }
