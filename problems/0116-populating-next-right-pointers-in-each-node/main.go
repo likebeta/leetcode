7e5a78e0986c23e531929ad7f8466afebbd500b4
likebeta <ixxoo.me@gmail.com>
2024-09-07 23:07:37 +0800

    up

--- problems/0116-populating-next-right-pointers-in-each-node/main.go
+++ problems/0116-populating-next-right-pointers-in-each-node/main.go
@@ -6,14 +6,22 @@ import (
 
 type Node = helper.Node
 
+/*****************
+        1
+      /   \
+     2     3
+    / \   / \
+   4   5 6   7
+*****************/
+
 // 填充每个节点的下一个右侧节点指针
 func connect(root *Node) *Node {
 	if root == nil {
 		return root
 	}
 
-	for leftmost := root; leftmost.Left != nil; leftmost = leftmost.Left {
-		for node := leftmost; node != nil; node = node.Next {
+	for leftMost := root; leftMost.Left != nil; leftMost = leftMost.Left {
+		for node := leftMost; node != nil; node = node.Next {
 			node.Left.Next = node.Right
 			if node.Next != nil {
 				node.Right.Next = node.Next.Left
@@ -21,6 +29,26 @@ func connect(root *Node) *Node {
 		}
 	}
 	return root
+}
+
+func connect2(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+
+	traverseConnect(root.Left, root.Right)
+	return root
+}
+
+func traverseConnect(left, right *Node) {
+	if left == nil || right == nil {
+		return
+	}
+
+	left.Next = right
+	traverseConnect(left.Left, left.Right)
+	traverseConnect(right.Left, right.Right)
+	traverseConnect(left.Right, right.Left)
 }
 
 func main() {
