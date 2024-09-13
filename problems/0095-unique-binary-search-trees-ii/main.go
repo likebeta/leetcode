96a45289452c08cb6a5b789d8b9c7d0eddb48b45
likebeta <ixxoo.me@gmail.com>
2024-09-13 22:29:10 +0800

    up

--- problems/0095-unique-binary-search-trees-ii/main.go
+++ problems/0095-unique-binary-search-trees-ii/main.go
@@ -22,14 +22,14 @@ func makeTrees(left, right int) []*TreeNode {
 		return ans
 	}
 	for i := left; i <= right; i++ {
-		head := &TreeNode{Val: i}
 		lTrees := makeTrees(left, i-1)
 		rTrees := makeTrees(i+1, right)
 		for l := range lTrees {
 			for r := range rTrees {
+				head := &TreeNode{Val: i}
 				head.Left = lTrees[l]
 				head.Right = rTrees[r]
-				ans = append(ans, cloneTree(head))
+				ans = append(ans, head)
 			}
 		}
 
@@ -37,25 +37,15 @@ func makeTrees(left, right int) []*TreeNode {
 	return ans
 }
 
-func cloneTree(head *TreeNode) *TreeNode {
-	if head == nil {
-		return head
-	}
-	node := &TreeNode{Val: head.Val}
-	node.Left = cloneTree(head.Left)
-	node.Right = cloneTree(head.Right)
-	return node
-}
-
 func main() {
 	for _, t := range generateTrees(0) {
 		helper.Log(t.Dump())
 	}
-	helper.Log()
+	helper.Log("###################")
 	for _, t := range generateTrees(1) {
 		helper.Log(t.Dump())
 	}
-	helper.Log()
+	helper.Log("###################")
 	for _, t := range generateTrees(3) {
 		helper.Log(t.Dump())
 	}
--- problems/0096-unique-binary-search-trees/main.go
+++ problems/0096-unique-binary-search-trees/main.go
@@ -8,7 +8,7 @@ import (
 func numTrees(n int) int {
 	// dp(n) = f(1) + f(2) + ... + f(i) + ... + f(n)
 	// f(i) = dp(i-1)*dp(n-i)
-	dp := make([]int, n+1, n+1)
+	dp := make([]int, n+1)
 	dp[0] = 1
 	for i := 1; i <= n; i++ {
 		for j := 1; j <= i; j++ {
@@ -18,7 +18,32 @@ func numTrees(n int) int {
 	return dp[n]
 }
 
+func numTrees2(n int) int {
+	memo := make(map[int]int, n)
+	return count(memo, 1, n)
+}
+
+func count(memo map[int]int, i, j int) int {
+	if i >= j {
+		return 1
+	}
+	if v, ok := memo[j-i]; ok {
+		return v
+	}
+	var ans int
+	for k := i; k <= j; k++ {
+		left := count(memo, i, k-1)
+		right := count(memo, k+1, j)
+		ans += left * right
+	}
+	memo[j-i] = ans
+	return ans
+}
+
 func main() {
 	helper.Assert(numTrees(3) == 5)
 	helper.Assert(numTrees(5) == 42)
+
+	helper.Assert(numTrees2(3) == 5)
+	helper.Assert(numTrees2(5) == 42)
 }
