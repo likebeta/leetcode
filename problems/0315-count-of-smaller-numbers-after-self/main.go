a7921f30ca044accb815cc144c1ba410aa244854
likebeta <ixxoo.me@gmail.com>
2025-03-10 16:59:47 +0800

    0315 - mergeSort

--- problems/0315-count-of-smaller-numbers-after-self/main.go
+++ problems/0315-count-of-smaller-numbers-after-self/main.go
@@ -6,16 +6,14 @@ import (
 )
 
 // 计算右侧小于当前元素的个数
-var a, c []int
-
 func countSmaller(nums []int) []int {
-	resultList := []int{}
-	discretization(nums)
-	c = make([]int, len(nums)+5)
+	var resultList []int
+	c := make([]int, len(nums)+5)
+	a := discretization(nums)
 	for i := len(nums) - 1; i >= 0; i-- {
-		id := getId(nums[i])
-		resultList = append(resultList, query(id-1))
-		update(id)
+		id := getId(a, nums[i])
+		resultList = append(resultList, query(c, id-1))
+		update(c, id)
 	}
 	for i := 0; i < len(resultList)/2; i++ {
 		resultList[i], resultList[len(resultList)-1-i] = resultList[len(resultList)-1-i], resultList[i]
@@ -27,14 +25,14 @@ func lowBit(x int) int {
 	return x & (-x)
 }
 
-func update(pos int) {
+func update(c []int, pos int) {
 	for pos < len(c) {
 		c[pos]++
 		pos += lowBit(pos)
 	}
 }
 
-func query(pos int) int {
+func query(c []int, pos int) int {
 	ret := 0
 	for pos > 0 {
 		ret += c[pos]
@@ -43,22 +41,90 @@ func query(pos int) int {
 	return ret
 }
 
-func discretization(nums []int) {
+func discretization(nums []int) []int {
 	set := map[int]struct{}{}
 	for _, num := range nums {
 		set[num] = struct{}{}
 	}
-	a = make([]int, 0, len(nums))
+	a := make([]int, 0, len(nums))
 	for num := range set {
 		a = append(a, num)
 	}
 	sort.Ints(a)
+	return a
 }
 
-func getId(x int) int {
+func getId(a []int, x int) int {
 	return sort.SearchInts(a, x) + 1
 }
 
+type pair struct {
+	i int // 下标
+	v int // 值
+}
+
+func countSmaller2(nums []int) []int {
+	n := len(nums)
+	arr := make([]pair, n)
+	for i := 0; i < n; i++ {
+		arr[i] = pair{i, nums[i]}
+	}
+	tmp := make([]pair, n)
+	count := make([]int, n)
+
+	mergeSort(tmp, arr, count, 0, n-1)
+
+	return count
+}
+
+func mergeSort(tmp, arr []pair, count []int, left, right int) {
+	if left < right {
+		mid := left + (right-left)/2
+		mergeSort(tmp, arr, count, left, mid)
+		mergeSort(tmp, arr, count, mid+1, right)
+		merge(tmp, arr, count, left, mid, right)
+	}
+}
+
+func merge(tmp, arr []pair, count []int, left, mid, right int) {
+	for i := left; i <= right; i++ {
+		tmp[i] = arr[i]
+	}
+
+	i, j := left, mid+1
+	for idx := left; idx <= right; idx++ {
+		if i > mid {
+			// 左侧已合并完
+			arr[idx], j = tmp[j], j+1
+		} else if j > right {
+			// 右侧已合并完
+			count[tmp[i].i] += j - (mid + 1)
+			arr[idx], i = tmp[i], i+1
+		} else if tmp[i].v > tmp[j].v {
+			// 左侧>右侧
+			arr[idx], j = tmp[j], j+1
+		} else {
+			// 左侧<=右侧
+			count[tmp[i].i] += j - (mid + 1)
+			arr[idx], i = tmp[i], i+1
+		}
+	}
+}
+
+func testOne(in string, ans string) {
+	{
+		nums := helper.ParseIntArray(in)
+		count := countSmaller(nums)
+		helper.Assert(helper.DumpIntArray(count) == ans)
+	}
+
+	{
+		nums := helper.ParseIntArray(in)
+		count := countSmaller2(nums)
+		helper.Assert(helper.DumpIntArray(count) == ans)
+	}
+}
+
 func main() {
-	helper.Log(countSmaller([]int{5,2,6,1}), []int{2,1,1,0})
+	testOne("[5,2,6,1]", "[2,1,1,0]")
 }
