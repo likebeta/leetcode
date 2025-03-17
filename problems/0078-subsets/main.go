e0aff3acf0b21d6e8d7462e7fd9aa1478275f61b
likebeta <ixxoo.me@gmail.com>
2025-03-17 16:13:22 +0800

    0078 - backtrack

--- problems/0078-subsets/main.go
+++ problems/0078-subsets/main.go
@@ -6,23 +6,43 @@ import (
 
 // 子集
 func subsets(nums []int) [][]int {
-	length := len(nums)
-	if length == 0 {
-		return nil
-	}
 	result := [][]int{{}}
-	for i := 0; i < length; i++ {
+	for i := 0; i < len(nums); i++ {
 		for j := len(result) - 1; j >= 0; j-- {
 			tmp := len(result[j])
-			one := make([]int, tmp+1, tmp+1)
-			one[tmp] = nums[i]
+			one := make([]int, tmp+1)
 			copy(one, result[j])
+			one[tmp] = nums[i]
 			result = append(result, one)
 		}
 	}
 	return result
 }
 
+func subsets2(nums []int) [][]int {
+	var (
+		trace     []int
+		result    [][]int
+		backtrack func(int)
+	)
+
+	backtrack = func(start int) {
+		tmp := make([]int, len(trace))
+		copy(tmp, trace)
+		result = append(result, tmp)
+		for i := start; i < len(nums); i++ {
+			trace = append(trace, nums[i])
+			backtrack(i + 1)
+			trace = trace[:len(trace)-1]
+		}
+	}
+
+	backtrack(0)
+
+	return result
+}
+
 func main() {
 	helper.Log(subsets([]int{1, 2, 3}))
+	helper.Log(subsets2([]int{1, 2, 3}))
 }
