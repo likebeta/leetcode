dd83fe8a5539be4165f31d5bcf2cef5dbe72ef0c
likebeta <ixxoo.me@gmail.com>
2025-03-17 20:11:36 +0800

    0040

--- problems/0040-combination-sum-ii/main.go
+++ problems/0040-combination-sum-ii/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"leetcode/helper"
+	"slices"
 	"sort"
 )
 
@@ -44,6 +45,36 @@ func combinationSum2(candidates []int, target int) (ans [][]int) {
 	return
 }
 
+func combinationSum2_2(candidates []int, target int) [][]int {
+	var (
+		ans       [][]int
+		backtrack func(int, int)
+		record    = make([]int, 0)
+	)
+
+	sort.Ints(candidates)
+
+	backtrack = func(i int, sum int) {
+		if sum > target {
+			return
+		} else if sum == target {
+			ans = append(ans, slices.Clone(record))
+			return
+		}
+
+		for j := i; j < len(candidates); j++ {
+			// [i, len(candidates)) 如果有相同元素只选择第一个， 因为选择第二个的情况被选择第一个包含了
+			if j == i || candidates[j] != candidates[j-1] {
+				record = append(record, candidates[j])
+				backtrack(j+1, sum+candidates[j])
+				record = record[:len(record)-1]
+			}
+		}
+	}
+	backtrack(0, 0)
+	return ans
+}
+
 func min(a, b int) int {
 	if a < b {
 		return a
@@ -53,5 +84,7 @@ func min(a, b int) int {
 
 func main() {
 	helper.Log(combinationSum2([]int{10, 1, 2, 7, 6, 1, 5}, 8))
+	helper.Log(combinationSum2_2([]int{10, 1, 2, 7, 6, 1, 5}, 8))
 	helper.Log(combinationSum2([]int{2, 5, 2, 1, 2}, 5))
+	helper.Log(combinationSum2_2([]int{2, 5, 2, 1, 2}, 5))
 }
