47483f87eb1f2312eecc98e372e294e42482709a
likebeta <ixxoo.me@gmail.com>
2025-03-18 19:36:12 +0800

    0111 - bfs

--- problems/0111-minimum-depth-of-binary-tree/main.go
+++ problems/0111-minimum-depth-of-binary-tree/main.go
@@ -26,16 +26,43 @@ func minDepth(root *TreeNode) int {
 	return ans + 1
 }
 
-func min(a, b int) int {
-	if a > b {
-		return b
+func minDepth2(root *TreeNode) int {
+	if root == nil {
+		return 0
 	}
-	return a
+	queue := []*TreeNode{root}
+	var depth int
+	for len(queue) > 0 {
+		depth++
+		var tmp []*TreeNode
+		for i := range len(queue) {
+			if queue[i].Left == nil && queue[i].Right == nil {
+				return depth
+			}
+
+			if queue[i].Left != nil {
+				tmp = append(tmp, queue[i].Left)
+			}
+			if queue[i].Right != nil {
+				tmp = append(tmp, queue[i].Right)
+			}
+		}
+		queue = tmp
+	}
+
+	return depth
 }
 
 func testOne(in string, ans int) {
-	t := helper.NewTree(in)
-	helper.Log(minDepth(t), ans)
+	{
+		t := helper.NewTree(in)
+		helper.Assert(minDepth(t) == ans)
+	}
+
+	{
+		t := helper.NewTree(in)
+		helper.Assert(minDepth2(t) == ans)
+	}
 }
 
 func main() {
@@ -43,4 +70,5 @@ func main() {
 	testOne("[1,2]", 2)
 	testOne("[1]", 1)
 	testOne("[1,null,2]", 2)
+	testOne("[1,2,3,4,5]", 2)
 }
